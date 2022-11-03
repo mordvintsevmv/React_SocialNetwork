f96164573bf6b21791118a578583ac9c05b764c7
Mordvintsevmv <mordvintsevmv@gmail.com>
2022-11-03 19:43:35 +0400

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='logoMonkey.ico' alt='monkey'/>
+        </header>
+
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+
+const Profile = () => {
+    return(
+        <div className='content'>
+            <div>
+                <img src='forest.jpg'/>
+            </div>
+
+            <div>
+                Avatar + Description
+            </div>
+
+            <div>
+                Posts
+
+                <div>
+                    New Post
+                </div>
+
+                <div>
+                    All Posts
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
