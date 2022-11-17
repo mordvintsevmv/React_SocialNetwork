d7551604fe604ac57618a5233236a11d3fe633f8
Mordvintsevmv <mordvintsevmv@gmail.com>
2022-11-18 03:03:35 +0400

    Refactoring useEffect Hook

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,10 +1,14 @@
 import ProfileInfoCSS from './ProfileInfo.module.css'
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 const ProfileStatus = (props) => {
 
     const [editMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
+
+    useEffect(()=>{
+        setStatus(props.status)
+    }, [props.status])
 
     const onStatusChange = (e) => {
         setStatus(e.currentTarget.value)
